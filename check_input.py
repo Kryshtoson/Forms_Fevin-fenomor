1e6ff0c128483bfbf543b391796a08b38382f24e
Kryshtoson <krystof.chytry@gmail.com>
2023-04-03 08:31:49 +0200

    March23 update

--- check_input.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-path1 = 'C:\\Users\\krystof\\OneDrive - MUNI\\Devin\\Fevin\\Fevin-2019-2023.xlsx'
-path2 = 'C:\\Users\\krystof\\OneDrive - MUNI\\Devin\\Fenomor\\Fenomor_2019-2022.xlsx'
-
-step2 = pd.read_excel(path1).groupby(['plot', 'month', 'year', 'species']).size().reset_index(name='noent')
-step2[step2['noent'] > 1]
