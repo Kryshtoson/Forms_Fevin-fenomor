1a57a36ea08503fa8d0cd0f6d2c742667efb2fef
Kryshtoson <krystof.chytry@gmail.com>
2023-07-17 15:33:09 +0200

    July run

--- 1_new_forms.py
+++ 1_new_forms.py
@@ -14,7 +14,7 @@ for x in ['Fevin', 'Fenomor']:
         print('specify source')
         sys.exit()
 
-    #os.startfile('C:\\Users\\krystof\\OneDrive - MUNI\\Devin\\Fenomor\\')
+    # os.startfile('C:\\Users\\krystof\\OneDrive - MUNI\\Devin\\Fenomor\\')
 
     # I call it fevin but it may be fenomor too in the script
     fevin = pd.read_excel(path, 'data')
@@ -29,12 +29,13 @@ for x in ['Fevin', 'Fenomor']:
 
     fevin['height'] = [str(i) for i in fevin['height']]
     fevin['date'] = pd.to_datetime(fevin['year'].astype(str) + fevin['month'].astype(str), format='%Y%B')
-    fevin['data'] = fevin['cover'].astype(str) + ' ' + fevin['height'].astype(str) + ' ' + fevin['phenology'].astype(str)
+    fevin['data'] = fevin['cover'].astype(str) + ' ' + fevin['height'].astype(str) + ' ' + fevin['phenology'].astype(
+        str)
 
     fevin = fevin.sort_values('date')
     last_two = fevin[['month', 'year']].drop_duplicates().tail(3)
 
-    fevin_last_two = pd.merge(fevin, last_two, how = 'inner')
+    fevin_last_two = pd.merge(fevin, last_two, how='inner')
 
     writer = pd.ExcelWriter('out\\' + x + '.xlsx', mode='w')
 
@@ -44,22 +45,22 @@ for x in ['Fevin', 'Fenomor']:
         step['cols'] = step['year'].astype(str) + '\n' + step['month'].astype(str)
         step = step.sort_values('date')
         original_order = step["cols"].unique()
-        out = step[['species', 'cols', 'data']].pivot(columns = 'cols', values = 'data', index = 'species')
-        out = out.reindex(columns = original_order)
-        #out2 = pd.DataFrame({'species': out.index})
-        #out2
+        out = step[['species', 'cols', 'data']].pivot(columns='cols', values='data', index='species')
+        out = out.reindex(columns=original_order)
+        # out2 = pd.DataFrame({'species': out.index})
+        # out2
         clname0 = out.columns.str.split('\n')[0][1][0:3] + '\n' + out.columns.str.split('\n')[0][0][2:4]
         clname1 = out.columns.str.split('\n')[1][1][0:3] + '\n' + out.columns.str.split('\n')[1][0][2:4]
         clname2 = out.columns.str.split('\n')[2][1][0:3] + '\n' + out.columns.str.split('\n')[2][0][2:4]
         clname0 = [clname0 + '\n' + str(i) for i in ['C', 'H', 'P']]
         clname1 = [clname1 + '\n' + str(i) for i in ['C', 'H', 'P']]
         clname2 = [clname2 + '\n' + str(i) for i in ['C', 'H', 'P']]
-        stuff = pd.concat([pd.concat([out[out.columns[0]].str.split(' ', expand = True),
-                                     out[out.columns[1]].str.split(' ', expand = True)], axis = 1),
-                          out[out.columns[2]].str.split(' ', expand=True)], axis = 1)
-        stuff.reset_index(inplace = True)
+        stuff = pd.concat([pd.concat([out[out.columns[0]].str.split(' ', expand=True),
+                                      out[out.columns[1]].str.split(' ', expand=True)], axis=1),
+                           out[out.columns[2]].str.split(' ', expand=True)], axis=1)
+        stuff.reset_index(inplace=True)
         stuff.columns = ['species'] + clname0 + clname1 + clname2
 
-        stuff.to_excel(writer, sheet_name= i, index=False)
+        stuff.to_excel(writer, sheet_name=i, index=False)
 
     writer.save()
